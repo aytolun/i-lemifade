e992ef52e0e87a6471ca243b0515bf4cb8e5b91e
aytolun <78691060+aytolun@users.noreply.github.com>
2021-02-20 12:34:43 +0000

    Add files via upload

---
+++ i-lemifade.c
@@ -0,0 +1,23 @@
+int işlem yapma #include <stdio.h> #include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+int a = 5;
+int b;
+int c,d;
+
+
+b = 15;
+
+c = a + b;
+
+d = c +10;
+
+printf("%d %d\n",  a, b);
+printf("%d %d\n", c, d);
+
+return 0;
+
+}
